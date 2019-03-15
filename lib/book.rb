5312fed406c2c9aaedd9aef23dc97cf6699dda50
Adam S <adam.sarli@gmail.com>
2019-03-15 17:12:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 class Book 
   def initialize(title="And Then There Were None")
     @title = title 
+    @author = "Agatha Christie"
   end 
-  attr_accessor :title 
+  attr_accessor :title, :author 
 end 
 
